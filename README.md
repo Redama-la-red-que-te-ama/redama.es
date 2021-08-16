03a750d002af1bc44c9938f40477336f67b2a746
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-08-16 11:57:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# www.redama.es
+# redama.es
---
+++ blog/test.c
@@ -0,0 +1,11 @@
+#include <stblib.h>
+#include <stdioh>
+
+int main (void){
+        puts("Status: 200 OK\r");
+        puts("Content-Type: text/html\r");
+        puts("\r");
+        puts("rotfl");
+        
+        return (EXIT_SUCCESS);
+}
